dc5f3c4bdf30526c4245c6f504da499f2f35d32f
Jeremy Park <37394334+jjp015@users.noreply.github.com>
2020-10-09 06:49:05 -0700

    Initial commit
    Forking for my own purpose

--- samples/photo_album/controllers/photos_controller.js
+++ samples/photo_album/controllers/photos_controller.js
@@ -19,43 +19,46 @@ function add_through_server(req, res) {
   Photo.count().then(function (amount) {
     photo.title = "My Photo #" + (amount + 1);
   })
-    .finally(function () {
-      res.render('photos/add', {
-        photo: photo
+      .finally(function () {
+        res.render('photos/add', {
+          photo: photo
+        });
       });
-    });
 }
 
 function create_through_server(req, res) {
-  // In through-the-server mode, the image is first uploaded to the server
-  // and from there to Cloudinary servers.
-  // The upload metadata (e.g. image size) is then added to the photo  model (photo.image)
-  // and then saved to the database.
+    // In through-the-server mode, the image is first uploaded to the server
+    // and from there to Cloudinary servers.
+    // The upload metadata (e.g. image size) is then added to the photo  model (photo.image)
+    // and then saved to the database.
 
-  // file was not uploaded redirecting to upload
-  if (req.files.image.ws.bytesWritten === 0) {
-    res.redirect('/photos/add');
-    return;
-  }
+    // file was not uploaded redirecting to upload
+    if (req.files.image.ws.bytesWritten === 0) {
+        res.redirect('/photos/add');
+        return;
+    }
 
-  var photo = new Photo(req.body);
-  // Get temp file path
-  var imageFile = req.files.image.path;
-  // Upload file to Cloudinary
-  cloudinary.uploader.upload(imageFile, { tags: 'express_sample' })
-    .then(function (image) {
-      console.log('** file uploaded to Cloudinary service');
-      console.dir(image);
-      photo.image = image;
-      // Save photo with image metadata
-      return photo.save();
+    var photo = new Photo(req.body);
+    // Get temp file path
+    var imageFile = req.files.image.path;
+    // Upload file to Cloudinary
+    cloudinary.uploader.upload(imageFile, {width: 500, height: 500, crop: "limit",
+        categorization: "google_tagging", auto_tagging: 0.7,    // Enable "Google Auto Tagging" Add-on in Cloudinary API
+        tags: "sample, demo, test, photo, cloudinary"
     })
-    .then(function () {
-      console.log('** photo saved');
-    })
-    .finally(function () {
-      res.render('photos/create_through_server', { photo: photo, upload: photo.image });
-    });
+        .then(function (image) {
+            console.log('** file uploaded to Cloudinary service');
+            console.dir(image);
+            photo.image = image;
+            // Save photo with image metadata
+            return photo.save();
+        })
+        .then(function () {
+            console.log('** photo saved');
+        })
+        .finally(function () {
+            res.render('photos/create_through_server', { photo: photo, upload: photo.image });
+        });
 }
 
 function add_direct(req, res) {
@@ -66,12 +69,12 @@ function add_direct(req, res) {
   Photo.count().then(function (amount) {
     photo.title = "My Photo #" + (amount + 1) + " (direct)";
   })
-    .finally(function () {
-      res.render('photos/add_direct', {
-        photo: photo,
-        cloudinary_cors: cloudinary_cors
+      .finally(function () {
+        res.render('photos/add_direct', {
+          photo: photo,
+          cloudinary_cors: cloudinary_cors
+        });
       });
-    });
 }
 
 function add_direct_unsigned(req, res) {
@@ -90,34 +93,34 @@ function add_direct_unsigned(req, res) {
   Photo.count().then(function (amount) {
     photo.title = "My Photo #" + (amount + 1) + " (direct unsigned)";
   })
-    .then(function () {
-      return cloudinary.api.upload_preset(preset_name);
-    })
-    .then(function (preset) {
-      if (!preset.settings.return_delete_token) {
-        return cloudinary.api.update_upload_preset(preset_name, { return_delete_token: true });
-      }
-      return undefined;
-    })
-    .catch(function (err) {
-      // Creating an upload preset is done here only for demo purposes.
-      // Usually it is created outside the upload flow via api or
-      // online console (https://cloudinary.com/console/settings/upload)
-      return cloudinary.api.create_upload_preset({
-        unsigned: true,
-        name: preset_name,
-        folder: "preset_folder",
-        return_delete_token: true
-      });
-    })
-    .finally(function (preset) {
-      res.render('photos/add_direct_unsigned',
-        {
-          photo: photo,
-          cloudinary_cors: cloudinary_cors,
-          preset_name: preset_name
+      .then(function () {
+        return cloudinary.api.upload_preset(preset_name);
+      })
+      .then(function (preset) {
+        if (!preset.settings.return_delete_token) {
+          return cloudinary.api.update_upload_preset(preset_name, { return_delete_token: true });
+        }
+        return undefined;
+      })
+      .catch(function (err) {
+        // Creating an upload preset is done here only for demo purposes.
+        // Usually it is created outside the upload flow via api or
+        // online console (https://cloudinary.com/console/settings/upload)
+        return cloudinary.api.create_upload_preset({
+          unsigned: true,
+          name: preset_name,
+          folder: "preset_folder",
+          return_delete_token: true
         });
-    });
+      })
+      .finally(function (preset) {
+        res.render('photos/add_direct_unsigned',
+            {
+              photo: photo,
+              cloudinary_cors: cloudinary_cors,
+              preset_name: preset_name
+            });
+      });
 }
 
 function create_direct(req, res) {
@@ -144,13 +147,13 @@ function create_direct(req, res) {
   photo.save().then(function () {
     console.log('** photo saved');
   })
-    .catch(function (err) {
-      result.error = err;
-      console.log('** error while uploading file');
-      console.dir(err);
-    }).finally(function () {
-      res.render('photos/create_direct', { photo: photo, upload: photo.image });
-    });
+      .catch(function (err) {
+        result.error = err;
+        console.log('** error while uploading file');
+        console.dir(err);
+      }).finally(function () {
+    res.render('photos/create_direct', { photo: photo, upload: photo.image });
+  });
 }
 
 module.exports.wire = function (app) {
